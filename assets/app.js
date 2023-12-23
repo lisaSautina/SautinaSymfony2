3557f075f24fc6019c45231a0011ae4aeacf2712
lizaSautina <sautina.liza2014@yandex.ru>
2023-12-23 12:19:13 +0700

    commit

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,4 +7,7 @@ import './bootstrap.js';
  */
 import './styles/app.css'
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
